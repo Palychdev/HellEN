57c8f565d759f1f6742cff052c1ca2bb9669b105
Palychdev <113437527+Palychdev@users.noreply.github.com>
2022-11-17 17:24:21 +0000

    Deploying to gh-pages from @ Palychdev/HellEN@11e325779e6dab47ac4df3da6e5a198890806ad9 🚀

---
+++ index.8c8b3c33.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper-container",{navigation:{nextEl:".right-arrow",prevEl:".left-arrow"},pagination:{el:".swiper-pagination",type:"bullets",clickable:!0,dynamicBullets:!0,dynamicMainBullets:3},loop:!0,spaceBetween:50,effect:"coverflow"});
+//# sourceMappingURL=index.8c8b3c33.js.map
